82d9aae4638b08692185c77c3fb0fdf13473a04e
GhifariADR <ghifariadr6@gmail.com>
2022-06-25 17:42:59 +0700

    Update fetchUser

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:hoqobajoe/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class EditProfilePage extends StatefulWidget {
@@ -16,11 +17,13 @@ class editUser {
   final String nama;
   final String email;
   final String role;
+  final String token;
 
   editUser({
     required this.nama,
     required this.email,
     required this.role,
+    required this.token,
   });
 
   factory editUser.fromJson(Map<String, dynamic> json) {
@@ -28,28 +31,38 @@ class editUser {
       nama: json['nama'],
       email: json['email'],
       role: json['role'],
+      token: json['token'],
     );
   }
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  final storage = const FlutterSecureStorage();
+
   Future<editUser> fetchUser() async {
+    var token = await storage.read(key: "TOKEN");
     final response = await http.get(
       Uri.parse('https://hoqobajoe.herokuapp.com/api/account'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token '
+      },
     );
 
     if (response.statusCode == 200) {
-      return editUser.fromJson(jsonDecode(response.body));
+      return editUser.fromJson(jsonDecode(response.body)['data']);
     } else {
       throw Exception('Failed to fetch User');
     }
   }
 
   Future<editUser> editProfileBTN(String nama, String email) async {
+    var token = await storage.read(key: "TOKEN");
     final response = await http.put(
       Uri.parse('https://hoqobajoe.herokuapp.com/api/update'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token '
       },
       body: jsonEncode(
         <String, String>{'nama': nama, 'email': email},
@@ -123,7 +136,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
       );
     }
 
-    Widget nameField() {
+    Widget nameField(String nama) {
       return Container(
         margin: const EdgeInsets.only(top: 50),
         child: Column(
@@ -153,7 +166,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                     Expanded(
                       child: TextFormField(
                         decoration: InputDecoration.collapsed(
-                          hintText: 'Muhammad Ghifari Adrian',
+                          hintText: nama,
                           hintStyle: hintTextStyle,
                         ),
                       ),
@@ -165,7 +178,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
       );
     }
 
-    Widget emailField() {
+    Widget emailField(String email) {
       return Container(
         margin: const EdgeInsets.only(top: 10),
         child: Column(
@@ -195,7 +208,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                   Expanded(
                     child: TextFormField(
                       decoration: InputDecoration.collapsed(
-                        hintText: 'ghifari@mail.com',
+                        hintText: email,
                         hintStyle: hintTextStyle,
                       ),
                     ),
@@ -230,7 +243,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
       );
     }
 
-    Widget showRole() {
+    Widget showRole(String role) {
       return Container(
         margin: const EdgeInsets.only(top: 10),
         child: Column(
@@ -259,7 +272,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                   ),
                   Expanded(
                     child: Text(
-                      'USER',
+                      role,
                       style: GoogleFonts.poppins(
                         fontSize: 12,
                         color: Colors.black.withOpacity(0.5),
@@ -282,14 +295,29 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: SafeArea(
         child: Container(
           margin: const EdgeInsets.only(top: 40.0, left: 10, right: 10),
-          child: Column(
-            children: [
-              profilePicture(),
-              nameField(),
-              emailField(),
-              showRole(),
-              editButton(),
-            ],
+          child: FutureBuilder(
+            future: fetchUser(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                editUser _profile = snapshot.data as editUser;
+                return Column(
+                  children: [
+                    profilePicture(),
+                    nameField(_profile.nama),
+                    emailField(_profile.email),
+                    showRole(_profile.role),
+                    editButton(),
+                  ],
+                );
+              }
+              return const Center(
+                child: SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
           ),
         ),
       ),
